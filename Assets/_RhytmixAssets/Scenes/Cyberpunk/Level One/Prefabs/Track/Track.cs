ebbb9ef7822c803d66e8a72c080d03029557a69f
Sonny Ponce <sonnyjponce@gmail.com>
2022-03-22 19:00:31 -0500

    Added object spawning in track and deleting itself, added temp UI

---
+++ Assets/_RhytmixAssets/Scenes/Cyberpunk/Level One/Prefabs/Track/Track.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+    [SerializeField] Transform[] SpawnTrans;
+    [SerializeField] GameObject[] EnvironmentToSpawn;
+    Transform _playerTransform;
+
+
+    private void Start()
+    {
+        _playerTransform = FindObjectOfType<BasicPlayer>().GetComponent<Transform>();
+        StartCoroutine(CheckIfPlayerCloseEnough());
+    }
+
+    private void SpawnEnvironment()
+    {
+        foreach(Transform trans in SpawnTrans)
+        {
+            GameObject randEnviroment = Instantiate(PickRandomEnvionment(),trans);
+        }
+    }
+    private GameObject PickRandomEnvionment()
+    {
+        int rand = Random.Range(0,EnvironmentToSpawn.Length);
+        return EnvironmentToSpawn[rand];
+    }
+    IEnumerator CheckIfPlayerCloseEnough()
+    {
+        while(true)
+        {
+            if(Vector3.Distance(_playerTransform.position,transform.position) < 100f)
+            {
+                SpawnEnvironment();
+                StartCoroutine(DeleteSelfWhenBehindPlayer());
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator DeleteSelfWhenBehindPlayer()
+    {
+        while (true)
+        {
+            if (Vector3.Distance(_playerTransform.position, transform.position) > 100f)
+            {
+                Destroy(gameObject);
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
