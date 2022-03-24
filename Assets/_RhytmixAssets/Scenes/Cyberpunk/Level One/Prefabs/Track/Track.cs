bef54490ab3f487f949d5c298551bf0425727464
Sonny Ponce <sonnyjponce@gmail.com>
2022-03-23 22:02:59 -0500

    Added more obj to track in cyberpunkLevelOne,updated Track script now have accentEnviorments,updated distance to spawn

--- Assets/_RhytmixAssets/Scenes/Cyberpunk/Level One/Prefabs/Track/Track.cs
+++ Assets/_RhytmixAssets/Scenes/Cyberpunk/Level One/Prefabs/Track/Track.cs
@@ -6,8 +6,12 @@ public class Track : MonoBehaviour
 {
     [SerializeField] Transform[] SpawnTrans;
     [SerializeField] GameObject[] EnvironmentToSpawn;
+
+    [SerializeField] bool HasAccentEnvironment = false;
+    [SerializeField] Transform[] AccentSpawnTrans;
+    [SerializeField] GameObject[] AccentEnvironmentToSpawn;
     Transform _playerTransform;
-    float _distanceToCheck = 150f;
+    [SerializeField] float DistanceToCheckToSpawn = 150f;
 
 
     private void Start()
@@ -20,19 +24,37 @@ public class Track : MonoBehaviour
     {
         foreach(Transform trans in SpawnTrans)
         {
-            GameObject randEnviroment = Instantiate(PickRandomEnvionment(),trans);
+            GameObject randEnviroment = Instantiate(PickRandomEnvironment(),trans);
+        }
+
+        if(HasAccentEnvironment)
+        {
+            //Random.value return a random value between 0-1 -- so 50% chance to spawn accentEnvironment
+            if (Random.value > 0.5)
+            {
+                foreach (Transform trans in AccentSpawnTrans)
+                {
+                    GameObject randAccentEnviroment = Instantiate(PickRandomAccentEnvironment(), trans);
+                }
+            }
         }
     }
-    private GameObject PickRandomEnvionment()
+    private GameObject PickRandomEnvironment()
     {
         int rand = Random.Range(0,EnvironmentToSpawn.Length);
         return EnvironmentToSpawn[rand];
     }
+    private GameObject PickRandomAccentEnvironment()
+    {
+        int rand = Random.Range(0, AccentEnvironmentToSpawn.Length);
+        return AccentEnvironmentToSpawn[rand];
+    }
+
     IEnumerator CheckIfPlayerCloseEnough()
     {
         while(true)
         {
-            if(Vector3.Distance(_playerTransform.position,transform.position) < _distanceToCheck)
+            if(Vector3.Distance(_playerTransform.position,transform.position) < DistanceToCheckToSpawn)
             {
                 SpawnEnvironment();
                 StartCoroutine(DeleteSelfWhenBehindPlayer());
@@ -46,7 +68,7 @@ public class Track : MonoBehaviour
     {
         while (true)
         {
-            if (Vector3.Distance(_playerTransform.position, transform.position) > _distanceToCheck)
+            if (Vector3.Distance(_playerTransform.position, transform.position) > DistanceToCheckToSpawn)
             {
                 Destroy(gameObject);
                 break;
