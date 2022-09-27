ff2bfcf28063c4a0421339b4e3d8159681997809
Sonny Ponce <sonnyjponce@gmail.com>
2022-09-27 18:47:14 -0500

    Added amber buildings, reorganize folder structurem, added new building sets for Alley Rat level

---
+++ Assets/_RhytmixAssets/Player/Scripts/BasicPlayer.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BasicPlayer : MonoBehaviour
+{
+    float _gravity = -9.8f;
+    CharacterController characterController;
+    HeathComponent healthComp;
+    bool isDead = false;
+    [SerializeField] Animator[] playerAnimators;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        healthComp = GetComponent<HeathComponent>();
+        healthComp.onDeath += Death;
+    }
+
+    private void Death()
+    {
+        if(!isDead)
+        {
+            //print("I Die");
+            isDead = false;
+            if(playerAnimators != null)
+            {
+                foreach(Animator animator in playerAnimators)
+                {
+                    print(animator.name);
+                    animator.SetTrigger("DeathTrigger");
+                    //animator.SetLayerWeight(1,0);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        Vector3 playerVelocity = Vector3.zero;
+
+        if (!IsOnGround())
+        {
+            playerVelocity.y = Time.deltaTime * _gravity;
+            //fix this
+            playerVelocity = AdjustVelocityToSlope(playerVelocity);
+        }
+        characterController.Move(playerVelocity);
+    }
+
+    private Vector3 AdjustVelocityToSlope(Vector3 velocity)
+    {
+        var ray = new Ray(transform.position, Vector3.down);
+        if ((Physics.Raycast(ray, out RaycastHit hitInfo, 0.2f)))
+        {
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up,hitInfo.normal);
+            var adjustedVelocity = slopeRotation * velocity;
+            Debug.Log(adjustedVelocity);
+            return adjustedVelocity;
+        }
+        return velocity;
+
+    }
+
+
+    private bool IsOnGround()
+    {
+        var ray = new Ray(transform.position, Vector3.down);
+        if ((Physics.Raycast(ray, out RaycastHit hitInfo, 0.2f)))
+        {
+            //Debug.Log(hitInfo.collider.gameObject.name);
+            return true;
+        }
+        return false;
+    }
+}
