aea60a584daff78e2513007a21dc16fa7850de90
Elaynie <elaynemeza01@gmail.com>
2023-02-15 10:04:27 -0600

    Cinematic update bobbing script

--- Assets/_RhytmixAssets/Framework/Note/SwipeUpNote/BobingScript.cs
+++ Assets/_RhytmixAssets/Framework/Note/SwipeUpNote/BobingScript.cs
@@ -6,14 +6,23 @@ public class BobingScript : MonoBehaviour
 {
     [SerializeField] float _speed;
     [SerializeField] float _height;
+    [SerializeField] bool bobYAxis;
+    Vector3 _startPos;
     private float _startPosZ;
-
+    private float _startPosY;
     private void Start()
     {
-        _startPosZ = transform.localPosition.z;
+        _startPos = transform.localPosition;
     }
     private void Update()
     {
-        transform.localPosition = new Vector3(0,0, _height * Mathf.Sin(Time.realtimeSinceStartup * _speed) + _startPosZ);
+        if(!bobYAxis)
+        {
+            transform.localPosition = new Vector3(_startPos.x,_startPos.y, _height * Mathf.Sin(Time.realtimeSinceStartup * _speed) + _startPosZ);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(_startPos.x, _height * Mathf.Sin(Time.realtimeSinceStartup * _speed) + _startPosY, _startPos.z);
+        }
     }
 }
