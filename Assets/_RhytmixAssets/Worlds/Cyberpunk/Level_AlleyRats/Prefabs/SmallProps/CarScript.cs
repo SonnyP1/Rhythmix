5b64d6dce1eb2fea57010e435c805dc8dbe61cc3
AnimationNeo <angelr0911@gmail.com>
2023-04-05 10:18:42 -0500

    Worked on ending cinematic
    Added lighting, cars, and animations to ending cinematic

--- Assets/_RhytmixAssets/Worlds/Cyberpunk/Level_AlleyRats/Prefabs/SmallProps/CarScript.cs
+++ Assets/_RhytmixAssets/Worlds/Cyberpunk/Level_AlleyRats/Prefabs/SmallProps/CarScript.cs
@@ -7,11 +7,37 @@ public class CarScript : MonoBehaviour
     [SerializeField] Transform car;
     [SerializeField] Transform pointOne;
     [SerializeField] Transform pointTwo;
-    [SerializeField] float timeToLerp;
+
+    [SerializeField] float speed1Lerp;
+    [SerializeField] float speed2Lerp;
+
+    [SerializeField] bool bHasTwoSpeeds;
+    [SerializeField] bool bLoop;
+
+    [SerializeField] float bobSpeed;
+    [SerializeField] float bobHeight;
     float time;
+    private float currentTimeToLerp;
+
+    private void Start()
+    {
+        currentTimeToLerp = speed1Lerp;
+    }
     void Update()
     {
         time += Time.deltaTime;
-        car.position = Vector3.Lerp(pointOne.position,pointTwo.position,time/timeToLerp);
+        car.position = Vector3.Lerp(pointOne.position,pointTwo.position,time/currentTimeToLerp);
+        car.position = new Vector3(car.position.x, car.position.y + Mathf.Sin(Time.realtimeSinceStartup * bobSpeed) * bobHeight, car.position.z);
+
+        if(bLoop && time/ currentTimeToLerp >= 1)
+        {
+            time = 0;
+            if(bHasTwoSpeeds)
+            {
+                if (currentTimeToLerp == speed1Lerp)
+                    currentTimeToLerp = speed2Lerp;
+                else currentTimeToLerp = speed1Lerp;
+            }
+        }
     }
 }
